cfffd55eb66e4e3dbd8db1d185769074caa8829a
natalie <natasha.glinkina2017@gmail.com>
2023-02-23 22:05:07 +0300

    seminar1

---
+++ seminar1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+/*
+Console.Write("input a first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = number2 * number2;
+
+if(number1 == quad2)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+*/
+
+
+// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+/*
+Console.Write("Input a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
