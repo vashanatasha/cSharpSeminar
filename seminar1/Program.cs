cd5311ba78d26410e5f009f75d11ca04c74f627d
natalie <natasha.glinkina2017@gmail.com>
2023-02-25 21:35:58 +0300

    дз задача 2

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -59,3 +59,20 @@ Console.Write("min = ");
 Console.WriteLine(min);
 */
 
+// Задача 4. Напишите программу, которая принимает 
+//на вход три числа и выдает максимальное из этих чисел.
+/*
+Console.Write("input a first number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input the third number ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+if (num1 > max) max = num1;
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+
