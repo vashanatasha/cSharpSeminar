6956f135db0d0f04b114c6886a8c4dcec5ae5396
natalie <natasha.glinkina2017@gmail.com>
2023-03-04 23:10:12 +0300

    seminar2 end

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -77,3 +77,63 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(IsQuad (num1, num2));
 */
+
+
+// Напишите программу, которая принимает на вход трехзначное 
+//число и на выходе показывает вторую цифру этого числа
+/*
+int Prompt (string message)
+{ //метод
+    System.Console.Write(message); //выводится строка "введите трхзн число"
+    string value = Console.ReadLine(); //новая переменная под ввод
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите трехзначное число > "); //делаем вызов и в скобках передаем значение, которое просится в 85 строчке
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, повторите ввод");
+    return;
+}
+Console.WriteLine($"Введенное число '{number}'");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра '{secondRank}'");
+*/
+
+// Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+
+// int Prompt(string message)
+// {
+//     Console.Write(message);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
+// }
+
+// int ThirdRank (int num)
+// {
+//     while (num > 999)
+//     {
+//         num /= 10;    //num = num/10
+//     }
+//     return num % 10;
+// }
+
+// bool ValidateNum (int num)
+// {
+//     if (num < 100)
+//     {
+//         Console.WriteLine("третьего числа нет");
+//         return false;
+//     }
+//     return true;
+// }
+
+
+// int num = Prompt("введите число ");
+// if (ValidateNum(num)) //валидатор подразумевает тру или фолс
+// {
+//     Console.WriteLine(ThirdRank(num));
+// }
