1690f7c248f9e32a4c6da52748097a5020ce0fba
natalie <natasha.glinkina2017@gmail.com>
2023-03-07 18:12:48 +0300

    Домашнее задание. семинар3

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -121,3 +121,66 @@ for(int i = 1; i <= number; i++)
     Console.Write($"{Math.Pow(i, 2)} ");
 }
 */
+
+
+
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine();
+// int num = number.Length;
+
+// if (num == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} - палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - не палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"{number} - не является пятизначным");
+// }
+
+
+// Задача 21 Напишите программу, которая принимает на вход координаты двух точек
+//  и находит расстояние между ними в 3D пространстве.
+
+// double Distance3D (double x1, double y1, double z1, double x2, double y2, double z2)
+// {
+//     double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+//     return Math.Round(res, 2);   
+// }
+// Console.Write("Введите первую точку оси X ");
+// double x1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите вторую точку оси X ");
+// double x2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите первую точку оси Y ");
+// double y1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите вторую точку оси Y ");
+// double y2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите первую точку оси Z ");
+// double z1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите вторую точку оси Z ");
+// double z2 = Convert.ToDouble(Console.ReadLine());
+
+// double distance = Distance3D(x1, y1, z1, x2, y2, z2);
+// System.Console.WriteLine($"Расстояние между точками = {distance }");
+
+
+// Задача 23. Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// Console.Write("Введите число: ");
+// double number = Convert.ToDouble(Console.ReadLine());
+
+// for(int i = 1; i <= number; i++)
+// {
+//     Console.Write($"{Math.Pow(i, 3)} ");
+// }
