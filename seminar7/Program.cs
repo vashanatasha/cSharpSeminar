0a896eed12527d27611fbc06d72e0eef7556977c
natalie <natasha.glinkina2017@gmail.com>
2023-03-24 13:19:57 +0300

    homework 7

---
+++ seminar7/Program.cs
@@ -0,0 +1,343 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, 
+// заполненный случайными целыми числами.
+// // m = 3, n = 4.
+// // 1 4 8 19
+// // 5 -2 33 -2
+// // 77 3 8 1
+
+// int[,] FillMatrixWithRandom(int row, int column)
+// {
+//     int[,] array = new int[row, column];
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = rnd.Next(0, 10);
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} \t");
+//         }
+//     Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите кол-во строк: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixWithRandom(row, column);
+// PrintMatrix(matrix);
+
+
+
+
+// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// int[,] FillMatrixWithRandom(int row, int column)
+// {
+//     int[,] array = new int[row, column];
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = i + j;
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} \t");
+//         }
+//     Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите кол-во строк: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixWithRandom(row, column);
+// PrintMatrix(matrix);
+
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+// int[,] FillMatrixWithRandom(int row, int column)
+// {
+//     int[,] array = new int[row, column];
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = rnd.Next(0, 10);
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] PowElements(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++) //i+=2
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++) //j+=2
+//         {
+//             if (i % 2 == 0 && j % 2 == 0)
+//             {
+//                 matrix[i, j] *= matrix[i, j];
+//             }
+//         }
+//     }
+//     return matrix;
+// }
+
+// Console.Write("Введите кол-во строк: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixWithRandom(row, column);
+// PrintMatrix(matrix);
+// PowElements(matrix);
+// System.Console.WriteLine();
+// PrintMatrix(matrix); //выводим матрицу, потому что она была изменина по ссылке
+
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+// int[,] FillMatrixWithRandom(int row, int column)
+// {
+//     int[,] array = new int[row, column];
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = rnd.Next(0, 10);
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int SumMainDiagonal(int[,] matrix)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i == j)
+//             {
+//                 sum += matrix[i, j];
+//             }
+//         }
+//     }
+//     return sum;
+// }
+// Console.Write("Введите кол-во строк: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixWithRandom(row, column);
+// PrintMatrix(matrix);
+// System.Console.WriteLine(SumMainDiagonal(matrix));
+
+
+
+// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+// Console.Write("Введите количество строк ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// double[,] numbers = new double[row, column];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// void FillArrayRandomNumbers(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = Convert.ToDouble(new Random().Next(-100, 1000))/100;
+//         }
+//     } 
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции 
+// элемента в двумерном массиве, и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
+
+// Console.Write("Введите кол-во строк: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите индекс строки: ");
+// int iRow = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите индекс столбца: ");
+// int iColumn = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixWithRandom(row, column);
+// PrintMatrix(matrix);
+
+
+// int[,] FillMatrixWithRandom(int row, int column)
+// {
+//     int[,] array = new int[row, column];
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = rnd.Next(0, 10);
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} \t");
+//         }
+//     Console.WriteLine();
+//     }
+// }
+// if (iRow < matrix.GetLength(0) && iColumn < matrix.GetLength(1)) 
+// Console.WriteLine(matrix[iRow, iColumn]);
+// else Console.WriteLine($"Такого числа в массиве нет");
+
+
+
+
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Console.Write("Введите кол-во строк: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixWithRandom(row, column);
+
+// int[,] FillMatrixWithRandom(int row, int column)
+// {
+//     int[,] array = new int[row, column];
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = rnd.Next(0, 10);
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// PrintMatrix(matrix);
+
+
+//     double saver = 0;
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         for (int i = 0; i < matrix.GetLength(0); i++)
+//         {
+//           saver = (saver + matrix[i, j]);
+//         }
+//         saver = Math.Round(saver/column, 2);
+        
+//         Console.Write(saver + "; ");
+//     }
+
+
+
+
+
